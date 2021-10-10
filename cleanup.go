c1675658cbd4391e4fc3ecb49e2d42078e7d3cbc
Richard Garai <garric@okki.hu>
2021-10-10 17:52:30 +0200

    remove logging from cleanup

--- cleanup.go
+++ cleanup.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"time"
 
 	"okki.hu/garric/ppnext/config"
@@ -13,7 +12,6 @@ func scheduleBackgroundCleanup() {
 	ch := time.NewTicker(consts.CleanupFrequency)
 	go func() {
 		for range ch.C {
-			log.Printf("performing cleanup")
 			config.Repository.Cleanup(consts.MaximumRoomAge)
 		}
 	}()
