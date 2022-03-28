b1117a928a572125256c8c25c79d796db36e363b
Richard Garai <garric@okki.hu>
2022-03-28 18:43:58 +0200

    mongo: Load

--- store/mongo.go
+++ store/mongo.go
@@ -1,13 +1,18 @@
 package store
 
 import (
+	"context"
 	"os"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"okki.hu/garric/ppnext/model"
 )
+
+const MongoDatabase = "ppnext"
+const MongoCollection = "rooms"
 
 type MongoRepository struct {
 	Client *mongo.Client
@@ -15,7 +20,7 @@ type MongoRepository struct {
 
 func NewMongoRepository() *MongoRepository {
 	uri := os.Getenv("MONGODB_URI")
-	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
@@ -24,8 +29,18 @@ func NewMongoRepository() *MongoRepository {
 	}
 }
 
-func (*MongoRepository) Load(name string) (*model.Room, error) {
-	return nil, nil
+func (m *MongoRepository) rooms() *mongo.Collection {
+	return m.Client.Database(MongoDatabase).Collection(MongoCollection)
+}
+
+func (m *MongoRepository) Load(name string) (*model.Room, error) {
+	var r model.Room
+	filter := bson.D{{
+		Key:   "name",
+		Value: name,
+	}}
+	err := m.rooms().FindOne(context.Background(), filter).Decode(&r)
+	return &r, err
 }
 
 func (*MongoRepository) Save(room *model.Room) error {
